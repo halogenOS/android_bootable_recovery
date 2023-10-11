6769a68906efb1f0399a90dc4be5d211421da974
Davide Garberi <dade.garberi@gmail.com>
2023-10-11 11:07:18 +0200

    recovery: Block downgrades and unsigned builds on locked bootloader
    Change-Id: I882c77b3ab06901e4cba8b22fe300068a0797062

--- recovery.cpp
+++ recovery.cpp
@@ -136,6 +136,10 @@ static bool IsRoDebuggable() {
   return true;
 }
 
+static bool IsDeviceUnlocked() {
+  return "orange" == android::base::GetProperty("ro.boot.verifiedbootstate", "");
+}
+
 // Clear the recovery command and prepare to boot a (hopefully working) system,
 // copy our log file to cache as well (for the system to read). This function is
 // idempotent: call it as many times as you like.
@@ -188,13 +192,21 @@ bool ask_to_ab_reboot(Device* device) {
 }
 
 bool ask_to_continue_unverified(Device* device) {
-  device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
-  return yes_no(device, "Signature verification failed", "Install anyway?");
+  if (!IsDeviceUnlocked()) {
+    return false;
+  } else {
+    device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+    return yes_no(device, "Signature verification failed", "Install anyway?");
+  }
 }
 
 bool ask_to_continue_downgrade(Device* device) {
-  device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
-  return yes_no(device, "This package will downgrade your system", "Install anyway?");
+  if (!IsDeviceUnlocked()) {
+    return false;
+  } else {
+    device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+    return yes_no(device, "This package will downgrade your system", "Install anyway?");
+  }
 }
 
 std::string get_preferred_fs(Device* device) {
