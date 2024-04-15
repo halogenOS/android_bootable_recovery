49c10d765786f525cbdb05f87cf587990250c534
Simão Gomes Viana <devel@superboring.dev>
2024-04-15 20:48:34 +0200

    Remove some annoyances
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- install/spl_check.cpp
+++ install/spl_check.cpp
@@ -30,16 +30,11 @@ bool ViolatesSPLDowngrade(const build::tools::releasetools::OtaMetadata& metadat
   // example, 05 for May). Otherwise this comparison doesn't work. We don't expect SPL date formats
   // to change, leave this as is.
   if (post_spl < current_spl) {
-    LOG(ERROR) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
+    LOG(WARNING) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
                << " this is considered a downgrade";
-    if (metadata.spl_downgrade() || metadata.downgrade()) {
-      LOG(WARNING)
-          << "SPL downgrade detected, but OTA package explicitly permitts this(OtaMetadata has "
-             "spl_downgrade / downgrade bit set).Permitting update anyway.Installing a SPL "
-             "downgrade OTA can cause /data fail to decrypt and device fails to boot.";
-      return false;
-    }
-    return true;
+    LOG(WARNING) << "Installing a SPL " <<
+	    "downgrade OTA can cause /data fail to decrypt and device fails to boot.";
+    return false;
   } else {
     LOG(INFO) << "old spl: " << current_spl << " new spl: " << post_spl << " CHECK passes";
   }
