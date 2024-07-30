96974f280873134e5e44c82742d9d1ccda310c91
N0m4n904 <noah.anleitner@halogenos.org>
2024-07-30 17:52:08 +0200

    Revert "recovery_ui: Add support for battery capacity status"
    This reverts commit fd4e64129d38909c90b7c060c08b797eb3140f15.

--- recovery_ui/include/recovery_ui/screen_ui.h
+++ recovery_ui/include/recovery_ui/screen_ui.h
@@ -32,7 +32,6 @@
 class GRSurface;
 
 enum class UIElement {
-  BATTERY_LOW,
   HEADER,
   MENU,
   MENU_SEL_BG,
@@ -393,7 +392,6 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   virtual std::vector<std::string> GetMenuHelpMessage() const;
 
   virtual void draw_background_locked();
-  virtual void draw_battery_capacity_locked();
   virtual void draw_foreground_locked();
   virtual void draw_screen_locked();
   virtual void draw_menu_and_text_buffer_locked(const std::vector<std::string>& help_message);
@@ -403,7 +401,6 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   const GRSurface* GetCurrentFrame() const;
   const GRSurface* GetCurrentText() const;
 
-  void BattMonitorThreadLoop();
   void ProgressThreadLoop();
 
   virtual void ShowFile(FILE*);
@@ -525,11 +522,6 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   bool rtl_locale_;
 
   std::mutex updateMutex;
-
-  std::thread batt_monitor_thread_;
-  std::atomic<bool> batt_monitor_thread_stopped_{ false };
-  int32_t batt_capacity_;
-  bool charging_;
 
   // Switch the display to active one after graphics is ready
   bool is_graphics_available;
