85defc688b3fc3efdc55aee5fa05cac5d6b3a6f8
Simão Gomes Viana <devel@superboring.dev>
2024-10-13 00:30:34 +0200

    install: build-tag mismatch should warn instead of error
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- install/install.cpp
+++ install/install.cpp
@@ -196,9 +196,8 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
     const auto& post_build_tag = build_fingerprint.back();
     const auto build_tag = android::base::GetProperty("ro.build.tags", "");
     if (build_tag != post_build_tag) {
-      LOG(ERROR) << "Post build-tag " << post_build_tag << " does not match device build tag "
-                 << build_tag;
-      return false;
+      LOG(WARNING) << "Post build-tag " << post_build_tag << " does not match device build tag "
+                 << build_tag << ". Mismatching keys may cause crashes!";
     }
   }
 
